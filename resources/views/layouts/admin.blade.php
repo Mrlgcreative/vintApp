ac52096963984c6d56dece6cec586fc538a84551
Sky_enfant_de_la_lumière <160028535+Mrlgcreative@users.noreply.github.com>
2025-12-05 00:28:53 +0200

    Fix: Ajout Font Awesome dans layout admin
    - Ajout du CDN Font Awesome 6.0.0 dans admin.blade.php
    - Les icônes fas fa-* s'affichent maintenant correctement
    - Cohérence avec app.blade.php qui charge déjà Font Awesome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,6 +33,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
+    
     <!-- Custom Page Styles -->
     @stack('styles')
     
