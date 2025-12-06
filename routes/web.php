8ce2057c2aa5bd133bf61abf79341a0b02a89db2
Sky_enfant_de_la_lumière <160028535+Mrlgcreative@users.noreply.github.com>
2025-12-06 02:34:48 +0200

    Fix: Désactiver route /dashboard pour éviter boucle redirection

--- routes/web.php
+++ routes/web.php
@@ -176,7 +176,8 @@ Route::get('/newsletter/track/open/{token}', [NewsletterController::class, 'trac
 Route::get('/newsletter/track/click/{token}', [NewsletterController::class, 'trackClick'])->name('newsletter.track.click');
 Route::get('/newsletter/track/click/{token}', [NewsletterController::class, 'trackClick'])->name('newsletter.track.click');
 
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+// Route dashboard commentée - utiliser /home à la place
+// Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 // Routes de test supprimées
 Route::get('/test-create', function() {
