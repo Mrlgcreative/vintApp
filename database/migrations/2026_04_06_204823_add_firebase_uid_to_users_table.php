d99af9924466d6803f9b7e1c5a43591d6cd241a2
Caleb <mrlgcreative@gmail.com>
2026-04-17 17:28:21 +0100

    v0.0.2c fix: suppression de d'une colone double

--- database/migrations/2026_04_06_204823_add_firebase_uid_to_users_table.php
+++ database/migrations/2026_04_06_204823_add_firebase_uid_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('firebase_uid')->nullable()->unique()->after('email');
+            if (!Schema::hasColumn('users', 'firebase_uid')) {
+                $table->string('firebase_uid')->nullable()->unique()->after('email');
+            }
         });
     }
 
