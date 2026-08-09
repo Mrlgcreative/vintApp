f90c82b3f944684bdc16a09f280e129dd7f46575
mrlgcreative <mrlgcreative@gmail.com>
2026-08-09 20:31:41 +0200

    fix(deploy): publie config/view.php sans realpath() pour le chemin des vues compilees
    Le config 'view.compiled' venait du framework et utilisait
    realpath(storage_path('framework/views')). Si le dossier est absent
    (apres un deploiement), realpath() renvoie false et le BladeCompiler
    echoue avec 'Please provide a valid cache path' (erreur 500).
    
    Avec une simple chaine storage_path(), Blade cree automatiquement le
    dossier lors de la premiere compilation : plus aucun 500 ni mkdir manuel
    apres deploiement.

---
+++ config/view.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your views. Of course
+    | the usual Laravel view path has already been registered for you.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where all the compiled Blade templates will be
+    | stored for your application. Typically, this is within the storage
+    | directory. However, as usual, you are free to change this value.
+    |
+    | NOTE : on évite realpath() ici. realpath() renvoie false quand le
+    | dossier n'existe pas encore, ce qui fait échouer le BladeCompiler
+    | ("Please provide a valid cache path") et provoque un 500 après un
+    | déploiement. Blade crée ce dossier automatiquement si le chemin est
+    | une simple chaîne valide.
+    |
+    */
+
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        storage_path('framework/views')
+    ),
+
+];
