ff463ac9d245583481a551bcf27ace924118f8cf
mrlgcreative <mrlgcreative@gmail.com>
2026-09-05 20:27:26 +0200

    Fix responsive WebView: wide viewport + overview mode sur petits écrans
    - setUseWideViewPort + setLoadWithOverviewMode pour respecter device-width
    - textZoom 100 pour garder la taille des textes
    - APK recopié

--- my-app/android/app/src/main/java/com/vintapp/app/MainActivity.java
+++ my-app/android/app/src/main/java/com/vintapp/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.vintapp.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fixWebViewSettings();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fixWebViewSettings();
+    }
+
+    private void fixWebViewSettings() {
+        try {
+            android.webkit.WebView webView = getBridge().getWebView();
+            WebSettings settings = webView.getSettings();
+            settings.setUseWideViewPort(true);
+            settings.setLoadWithOverviewMode(true);
+            settings.setTextZoom(100);
+        } catch (Exception e) {
+            // WebView pas encore prête
+        }
+    }
+}
