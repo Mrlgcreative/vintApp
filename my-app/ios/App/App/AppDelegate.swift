c51aaa1355686c90af42b8ba2be975eef23cd6d4
mrlgcreative <mrlgcreative@gmail.com>
2026-09-05 22:17:50 +0200

    Ajout plateforme iOS (Capacitor) + notifications push FCM iOS
    - npx cap add ios : projet Xcode (com.vintapp.app), AppIcon/VintApp officiel 1024px
    - AppDelegate.swift : FirebaseApp.configure() + forwarding APNs version @capacitor-firebase/messaging + Auth canHandle
    - VintApp.entitlements (aps-environment) référencé dans Debug + Release
    - Info.plist : UIBackgroundModes remote-notification
    - capacitor.config.json : SPM symlink pour @capacitor-firebase/messaging (évite collision d'identité SwiftPM)
    - Package.swift généré

---
+++ my-app/ios/App/App/AppDelegate.swift
@@ -0,0 +1,62 @@
+import UIKit
+import Capacitor
+import FirebaseCore
+import FirebaseMessaging
+import FirebaseAuth
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Configure Firebase (GoogleService-Info.plist requis dans le bundle)
+        FirebaseApp.configure()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+    // Forwarder du token de notification pour @capacitor-firebase/messaging
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "didReceiveRemoteNotification"), object: completionHandler, userInfo: userInfo)
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        if Auth.auth().canHandle(url) {
+            return true
+        }
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default Configuration",
+                                          sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+}
